b4d710a56ec845c118114099a9b233cc1a86515b
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-03 14:47:54 +0000

    MakeScale along arbitrary direction vector

--- MathsLibrary/Matrix.h
+++ MathsLibrary/Matrix.h
@@ -222,5 +222,19 @@ Matrix3x3 MakeScale(float sx, float sy, float sz) {
 					 0.0f, 0.0f, sz);
 }
 
+Matrix3x3 MakeScale(float s, const Vector3& a) {
+	s -= 1.0f;
+	float x = a.x * s;
+	float y = a.y * s;
+	float z = a.z * s;
+	float axay = x * a.y;
+	float axaz = x * a.z;
+	float ayaz = y * a.z;
+	
+	return Matrix3x3(x * a.x + 1.0f, axay, axaz,
+					 axay, y * a.y + 1.0f, ayaz,
+					 axaz, ayaz, z * a.z + 1.0f);
+}
+
 using Mat3 = Matrix3x3;
 using Mat4 = Matrix4x4;
