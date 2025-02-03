378c604235a5d28d4484de19394cd0dcca4bf9bd
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-03 14:28:01 +0000

    Implemented MakeReflection function returning Matrix3x3

--- MathsLibrary/Matrix.h
+++ MathsLibrary/Matrix.h
@@ -190,5 +190,18 @@ Matrix3x3 MakeRotation(const float t, const Vector3& a) {
 					 axaz - s * a.y, ayaz + s * a.x, c + z * a.z);
 }
 
+Matrix3x3 MakeReflection(const Vector3& a) {
+	float x = a.x * -2.0f;
+	float y = a.y * -2.0f;
+	float z = a.z * -2.0f;
+	float axay = x * a.y;
+	float axaz = x * a.z;
+	float ayaz = y * a.z;
+
+	return Matrix3x3(x * a.x + 1.0f, axay, axaz,
+					 axay, y * a.y + 1.0f, ayaz,
+					 axaz, ayaz, z * a.z + 1.0f);
+}
+
 using Mat3 = Matrix3x3;
 using Mat4 = Matrix4x4;
