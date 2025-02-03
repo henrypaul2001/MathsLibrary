5b0918d33eeb48767d968f75268ad35df53e3417
henrypaul2001 <henryp1.hp@googlemail.com>
2025-02-03 15:08:29 +0000

    MakeSkew function

--- MathsLibrary/Matrix.h
+++ MathsLibrary/Matrix.h
@@ -236,5 +236,16 @@ Matrix3x3 MakeScale(float s, const Vector3& a) {
 					 axaz, ayaz, z * a.z + 1.0f);
 }
 
+Matrix3x3 MakeSkew(float t, const Vector3& a, const Vector3& b) {
+	t = tan(t);
+	float x = a.x * t;
+	float y = a.y * t;
+	float z = a.z * t;
+
+	return Matrix3x3(x * b.x + 1.0f, x * b.y, x * b.z,
+					 y * b.x, y * b.y + 1.0f, y * b.z,
+					 z * b.x, z * b.y, z * b.z + 1.0f);
+}
+
 using Mat3 = Matrix3x3;
 using Mat4 = Matrix4x4;
